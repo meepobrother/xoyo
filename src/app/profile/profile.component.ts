d10bec352f2a6e4fae71e6908d80ce0222117354
bear_c <1639501830@qq.com>
2019-06-12 16:14:07 +0800

    个人中心填充完毕

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,8 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  featureList: { picture: string; desc: string; }[];
+  constructor() {
+    this.featureList = [{
+      picture: '//xoyo.xoyocdn.com/mobile/img/icon-balance.08efc043.png',
+      desc: '余额查询',
+    }, {
+      picture: '//xoyo.xoyocdn.com/mobile/img/icon-charge-record.acacbd0e.png',
+      desc: '充值记录',
+    }, {
+      picture: '//xoyo.xoyocdn.com/mobile/img/icon-identity-auth.04226371.png',
+      desc: '实名认证',
+    }, {
+      picture: '//xoyo.xoyocdn.com/mobile/img/icon-change-password.62e62960.png',
+      desc: '修改密码',
+    }, {
+      picture: '//xoyo.xoyocdn.com/mobile/img/icon-security.4921bf7c.png',
+      desc: '安全设置',
+    }];
+  }
 
   ngOnInit() {
   }
